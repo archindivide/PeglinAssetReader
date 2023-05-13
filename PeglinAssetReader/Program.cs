1c6a5cf5db9407dcba35f49eea48e10a7c3b0409
Matthew Jones <matthew.jones@marquam.com>
2023-05-13 12:31:08 -0500

    Add project files.

---
+++ PeglinAssetReader/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.Diagnostics;
+
+//Run AssetRipper to extract Peglin files from compiled code
+var peglinPath = ConfigurationManager.AppSettings["PeglinFolder"];
+var assetRipperPath = ConfigurationManager.AppSettings["AssetRipperApplication"];
+var outputPath = Environment.CurrentDirectory + "/output";
+
+var proc = new Process();
+proc.StartInfo.FileName = assetRipperPath;
+proc.StartInfo.Arguments = $"{peglinPath} -o {outputPath}";
+proc.Start();
+proc.WaitForExit();
+var exitCode = proc.ExitCode;
+proc.Close();
+
+Console.WriteLine(exitCode);
+
+//Parse language file
+
+//Parse asset files
+
+//Compile asset/image/language info together into single object
+
+//output a nice html page with sprites/descriptions
